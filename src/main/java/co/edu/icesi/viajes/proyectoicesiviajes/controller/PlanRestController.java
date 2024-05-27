6f3192027d95bd57a454bd9fbd12980464b11dae
Salazq <jc.salazarquintero@gmail.com>
2024-05-27 17:41:12 -0500

    Add plan registration

--- src/main/java/co/edu/icesi/viajes/proyectoicesiviajes/controller/PlanRestController.java
+++ src/main/java/co/edu/icesi/viajes/proyectoicesiviajes/controller/PlanRestController.java
@@ -2,6 +2,10 @@ package co.edu.icesi.viajes.proyectoicesiviajes.controller;
 
 import co.edu.icesi.viajes.proyectoicesiviajes.dto.ClientDTO;
 import co.edu.icesi.viajes.proyectoicesiviajes.dto.PlanDTO;
+import co.edu.icesi.viajes.proyectoicesiviajes.dto.PlanDetailDTO;
+import co.edu.icesi.viajes.proyectoicesiviajes.dto.PlanDetail_PlanDTO;
+import co.edu.icesi.viajes.proyectoicesiviajes.service.PlanDetailService;
+import co.edu.icesi.viajes.proyectoicesiviajes.service.PlanDetail_PlanService;
 import co.edu.icesi.viajes.proyectoicesiviajes.service.PlanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -18,6 +22,12 @@ public class PlanRestController {
     @Autowired
     private PlanService service;
 
+    @Autowired
+    private PlanDetailService serviceDetail;
+
+    @Autowired
+    private PlanDetail_PlanService servicePlanDetail;
+
     @GetMapping(path = "/all")
     @PreAuthorize("hasAnyAuthority('ADMIN')")
     public List<PlanDTO> getAll() {
@@ -26,10 +36,22 @@ public class PlanRestController {
 
     @PostMapping(path = "/create")
     @PreAuthorize("hasAnyAuthority('ADMIN')")
-    public PlanDTO createClient(@RequestBody PlanDTO plan) throws Exception {
+    public PlanDTO createPlan(@RequestBody PlanDTO plan) throws Exception {
         System.out.println(plan);
-
-        //long randomPositiveLong = Math.abs(UUID.randomUUID().getLeastSignificantBits());
         return service.save(plan);
+    }
+
+    @PostMapping(path = "/createdatails")
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
+    public PlanDetailDTO createDetails(@RequestBody PlanDetailDTO detail) throws Exception {
+        System.out.println(detail);
+        return serviceDetail.save(detail);
+    }
+
+    @PostMapping(path = "/createplandatails")
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
+    public PlanDetail_PlanDTO createPlanDetails(@RequestBody PlanDetail_PlanDTO detail) throws Exception {
+        System.out.println(detail);
+        return servicePlanDetail.save(detail);
     }
 }
